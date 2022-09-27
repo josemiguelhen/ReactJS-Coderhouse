2a3a3de4a56cb47c01cf4a9b9da9fcac7ce6adf8
Isidro Molina <isidromolina260@gmail.com>
2022-09-27 20:29:53 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
-import {NavBar} from './components/NavBar/NavBar'
+import { NavBar } from './components/NavBar/NavBar'
+import {ItemListContainer} from "./components/ItemListContainer/ItemListContainer"
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar />
+      <ItemListContainer title="listado" greeting="curso react"/>
     </div>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,21 @@
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
+import "./NavBar.css"
+// import Chango from "../../assets/chango.png"
+import { CartWidget } from '../CartWidget/CartWidget';
 
 export const NavBar = () => {
     return (
-        <nav>
-            menu de navegacion
-            <Button>boton BS</Button>
+        <nav className='navegacion'>
+            {/* <img src={xxxx}></img> */}
+            <img className='logo' src='https://i.pinimg.com/originals/0d/cf/b5/0dcfb548989afdf22afff75e2a46a508.jpg' alt='logo'></img>
+            <ul className='list'>
+                <li><a href="#">inicio</a></li>
+                <li><a href="#">camisas</a></li>
+                <li><a href="#">zapatos</a></li>
+                <li><a href="#">chomba</a></li>
+            </ul> 
+            {/* <Button className='chango'><img className='chango' src={Chango} alt="" /></Button> */}
+            <CartWidget/>
         </nav>
     )
 }
